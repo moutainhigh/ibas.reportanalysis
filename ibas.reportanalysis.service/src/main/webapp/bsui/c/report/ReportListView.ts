4f205a4932ee49e0241a6140060842f926f4cdaa
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-11 12:38:41 +0800

    更新工具条控件设置

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
@@ -84,8 +84,8 @@ export class ReportListView extends ibas.BOListView implements IReportListView {
         this.form.addContent(this.table);
         this.page = new sap.m.Page("", {
             showHeader: false,
-            subHeader: new sap.m.Bar("", {
-                contentLeft: [
+            subHeader: new sap.m.Toolbar("", {
+                content: [
                     new sap.m.Button("", {
                         text: ibas.i18n.prop("shell_data_new"),
                         type: sap.m.ButtonType.Transparent,
@@ -116,7 +116,7 @@ export class ReportListView extends ibas.BOListView implements IReportListView {
                             );
                         }
                     }),
-                    // new sap.m.ToolbarSeparator(""),// 加了后面不显示？
+                    new sap.m.ToolbarSeparator(""),
                     new sap.m.Button("", {
                         text: ibas.i18n.prop("shell_data_delete"),
                         type: sap.m.ButtonType.Transparent,
@@ -128,8 +128,7 @@ export class ReportListView extends ibas.BOListView implements IReportListView {
                             );
                         }
                     }),
-                ],
-                contentRight: [
+                    new sap.m.ToolbarSpacer(""),
                     new sap.m.Button("", {
                         type: sap.m.ButtonType.Transparent,
                         icon: "sap-icon://action",
