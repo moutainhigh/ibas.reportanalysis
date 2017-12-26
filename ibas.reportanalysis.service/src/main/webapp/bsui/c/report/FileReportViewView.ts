300573677bc96f851fce12e302b29597d85b1ac4
牛加人等于朱 <niuren.zhu@icloud.com>
2017-12-26 15:20:23 +0800

    移出不在使用的库

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/FileReportViewView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/FileReportViewView.ts
@@ -5,8 +5,6 @@
  * Use of this source code is governed by an Apache License, Version 2.0
  * that can be found in the LICENSE file at http://www.apache.org/licenses/LICENSE-2.0
  */
-
-/// <reference path="../../../3rdparty/swfobject" />
 import * as ibas from "ibas/index";
 import * as openui5 from "openui5/index";
 import * as bo from "../../../borep/bo/index";
