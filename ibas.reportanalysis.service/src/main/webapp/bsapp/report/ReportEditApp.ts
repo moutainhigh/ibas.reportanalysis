7cab279e8c6a4eb20c0d9aa54c030cb94b59fca6
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-05 11:25:36 +0800

    编辑对象时，新建对象直接使用

--- ibas.reportanalysis.service/src/main/webapp/bsapp/report/ReportEditApp.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsapp/report/ReportEditApp.ts
@@ -57,8 +57,15 @@ export class ReportEditApp extends ibas.BOEditApplication<IReportEditView, bo.Re
     run(): void {
         let that: this = this;
         if (ibas.objects.instanceOf(arguments[0], bo.Report)) {
+            let data: bo.Report = arguments[0];
+            // 新对象直接编辑
+            if (data.isNew) {
+                that.editData = data;
+                that.show();
+                return;
+            }
             // 尝试重新查询编辑对象
-            let criteria: ibas.ICriteria = arguments[0].criteria();
+            let criteria: ibas.ICriteria = data.criteria();
             if (!ibas.objects.isNull(criteria) && criteria.conditions.length > 0) {
                 // 有效的查询对象查询
                 let boRepository: BORepositoryReportAnalysis = new BORepositoryReportAnalysis();
--- ibas.reportanalysis.service/src/main/webapp/bsapp/reportbook/ReportBookEditApp.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsapp/reportbook/ReportBookEditApp.ts
@@ -56,8 +56,15 @@ export class ReportBookEditApp extends ibas.BOEditApplication<IReportBookEditVie
     run(): void {
         let that: this = this;
         if (ibas.objects.instanceOf(arguments[0], bo.ReportBook)) {
+            let data: bo.ReportBook = arguments[0];
+            // 新对象直接编辑
+            if (data.isNew) {
+                that.editData = data;
+                that.show();
+                return;
+            }
             // 尝试重新查询编辑对象
-            let criteria: ibas.ICriteria = arguments[0].criteria();
+            let criteria: ibas.ICriteria = data.criteria();
             if (!ibas.objects.isNull(criteria) && criteria.conditions.length > 0) {
                 // 有效的查询对象查询
                 let boRepository: BORepositoryReportAnalysis = new BORepositoryReportAnalysis();
