4c26f37d384ea628c7850e7967d2b88034980853
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-19 14:05:46 +0800

    列表去除无效的代码

--- ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsui/c/report/ReportListView.ts
@@ -182,11 +182,6 @@ export class ReportListView extends ibas.BOListView implements IReportListView {
         });
         return this.page;
     }
-    /** 嵌入查询面板 */
-    embedded(view: any): void {
-        this.page.addHeaderContent(view);
-        this.page.setShowHeader(true);
-    }
     private page: sap.m.Page;
     private form: sap.ui.layout.form.SimpleForm;
     private table: sap.ui.table.Table;
