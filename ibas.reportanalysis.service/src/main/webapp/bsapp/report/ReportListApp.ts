d390837b5ee34ca512914ab894c0420c0f024329
niurenzhu <niuren.zhu@icloud.com>
2018-01-20 16:28:59 +0800

    提示选择删除的数据

--- ibas.reportanalysis.service/src/main/webapp/bsapp/report/ReportListApp.ts
+++ ibas.reportanalysis.service/src/main/webapp/bsapp/report/ReportListApp.ts
@@ -137,6 +137,9 @@ export class ReportListApp extends ibas.BOListApplication<IReportListView, bo.Re
         }
         // 没有选择删除的对象
         if (beDeleteds.length === 0) {
+            this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_please_chooose_data",
+                ibas.i18n.prop("shell_data_delete")
+            ));
             return;
         }
         let that: this = this;
