80b7fb25d1dcd136c6bab0c92ef3e0f4e6e1f4e9
牛加人等于朱 <niuren.zhu@icloud.com>
2018-07-30 17:31:54 +0800

    优化代码

--- ibas.reportanalysis.service/src/main/java/org/colorcoding/ibas/reportanalysis/service/rest/Resolver.java
+++ ibas.reportanalysis.service/src/main/java/org/colorcoding/ibas/reportanalysis/service/rest/Resolver.java
@@ -6,9 +6,11 @@ import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-import org.colorcoding.ibas.bobas.bo.*;
-import org.colorcoding.ibas.bobas.common.*;
-import org.colorcoding.ibas.reportanalysis.bo.report.*;
+import org.colorcoding.ibas.bobas.bo.UserFieldProxy;
+import org.colorcoding.ibas.bobas.common.Criteria;
+import org.colorcoding.ibas.bobas.common.OperationResult;
+import org.colorcoding.ibas.reportanalysis.bo.report.Report;
+import org.colorcoding.ibas.reportanalysis.bo.reportbook.ReportBook;
 
 /**
  * 序列化解释器
@@ -17,20 +19,18 @@ import org.colorcoding.ibas.reportanalysis.bo.report.*;
 @Produces({ "application/json" })
 public class Resolver implements ContextResolver<JAXBContext> {
 
-    private static JAXBContext jaxbContext = null;
+	private static JAXBContext jaxbContext = null;
 
-    public JAXBContext getContext(Class<?> type) {
-        try {
-            if (jaxbContext == null) {
-                jaxbContext = JAXBContext.newInstance(Criteria.class, OperationResult.class
-                    , UserFieldProxy.class
-                    , Report.class
-                );
-            }
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-        return jaxbContext;
-    }
+	public JAXBContext getContext(Class<?> type) {
+		try {
+			if (jaxbContext == null) {
+				jaxbContext = JAXBContext.newInstance(Criteria.class, OperationResult.class, UserFieldProxy.class,
+						Report.class, ReportBook.class);
+			}
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return jaxbContext;
+	}
 
 }
